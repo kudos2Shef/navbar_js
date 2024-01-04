2f2a5973e0c7f098d6e3e68b298a945520504473
kudos2Shef <16985060+kudos2Shef@users.noreply.github.com>
2024-01-04 16:17:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click',function () {
-   // console.log('dcd',links.classList);
-   //console.log(links.classList.contains('links'));
-      //console.log(links.classList.contains('random'));
-  
-/* method1 for toggle
-   if(links.classList.contains('show-links')){
-   links.classList.remove('show-links');
-   //console.log('remove');
-   }
 
-   else{
-    links.classList.add('show-links');
-    //console.log('add');
-   }*/
 
    //method2 for toggle
    links.classList.toggle('show-links');
